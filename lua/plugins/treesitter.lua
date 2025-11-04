b85c78e9cb5e77e4c05445f4141d4df5d9594cf1
Maryush Soroka <sorokamario@gmail.com>
2025-11-04 20:21:20 +0300

    Disable treesitter on big files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -15,6 +15,13 @@ return {
       additional_vim_regex_highlighting = { 'ruby' },
     },
     indent = { enable = true, disable = { 'ruby' } },
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
   },
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
